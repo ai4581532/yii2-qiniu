8187210fdb80115bdf49e41e46add1e9910fe74d
charley <505437320@qq.com>
2018-08-29 11:58:58 +0800

    delete keys

--- config/params.php
+++ config/params.php
@@ -4,8 +4,8 @@ return [
     'adminEmail' => 'admin@example.com',
     
     //qiniu-sdk
-    'qiniu-accessKey'=>'bysXxZxahQzA3C-nbtymUee5g4dzDrMUnHv8Tefn',
-    'qiniu-secretKey'=>'GzryqQrMYL8ybyj6ugfP2WtBhtUPZGAJ8ccAh-cA',
+    'qiniu-accessKey'=>'',//此处请填写对应的key
+    'qiniu-secretKey'=>'',//此处请填写对应的key
     'qiniu-bucket'=>'freebucket',
     'qiniu-callbackUrl'=>'http://ppshny.natappfree.cc/basic/web/index.php/qiniu/uploadcallback',
     
